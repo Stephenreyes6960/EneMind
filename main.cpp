9032c680b281f9b92de466f32d74b5ef88e7259c
Stephenreyes6960 <44839586+Stephenreyes6960@users.noreply.github.com>
2018-11-07 13:01:43 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char **argv)
+{
+ QApplication app (argc, argv);
+
+ QPushButton button ("Why do you all sound like peanut's parents? !");
+ button.show();
+
+ return app.exec();
+}
+
+
+
